ead9c85bbb957bd307bdab11ed93bade57ce1e42
Matt Christensen <matt@LE-DD3V-B225.ads.warwick.ac.uk>
2024-03-27 13:40:52 +0000

    Made the plots neater

--- Landau_Damping.py
+++ Landau_Damping.py
@@ -18,11 +18,11 @@ matrix = np.diag(a,1) + np.diag(c,-1)
 # Alter the first upper and lower diagonal entries
 matrix[0,1] = matrix[1,0] = (1 + k0**(-2))**0.5
 
-print(matrix)
-
+# Calculate eigenvalues and eigenvectors
 eigval = np.linalg.eig(matrix)[0]
 eigvec = np.linalg.eig(matrix)[1]
 
+# Calculate total field
 tot = 0
 
 t = np.linspace(0,20,100)
@@ -30,33 +30,22 @@ t = np.linspace(0,20,100)
 for i in range(n+1):
     tot += (1j*alpha)/(2*k0)*eigvec[:,i][0]**2 *np.exp(1j*k0*eigval[i]*t)
 
+# Find the peaks in the oscillations
 peaks, _ = find_peaks(abs(tot))
 
+# Fit a line to the log of the peaks
 coefficients = np.polyfit(t[peaks], np.log(abs(tot)[peaks]), 1)
-
-plt.plot(t, abs(tot))
-plt.plot(t[peaks], abs(tot)[peaks], 'x')
-plt.xlabel('t')
-plt.ylabel('tot')
-plt.title('Plot of tot against t')
-plt.show()
-
-
-plt.plot(t[peaks], np.log(abs(tot)[peaks]), 'x')   
-plt.plot(t, coefficients[0]*t + coefficients[1])  
-plt.xlabel('t')
-plt.ylabel('log(tot)')
-plt.show()
 
 print('Coefficients', coefficients)
 
+# Plot the results
 fig, axs = plt.subplots(1, 2, figsize=(10, 5))
 
 axs[0].plot(t, abs(tot))
 axs[0].plot(t[peaks], abs(tot)[peaks], 'x')
 axs[0].set_xlabel('t')
 axs[0].set_ylabel('E')
-axs[0].set_title('Plot of tot against t')
+axs[0].set_title('Plot of E against t')
 
 axs[1].plot(t[peaks], np.log(abs(tot)[peaks]), 'x')
 axs[1].plot(t, coefficients[0]*t + coefficients[1])
