82b4547497fe5e1d33682adbb6ff8bf9474fc387
Matt Christensen <matthew.christensen@warwick.ac.uk>
2024-07-23 09:50:55 +0100

    Attempt at finite difference KvN method

--- KvN/KvN_tools_md.py
+++ KvN/KvN_tools_md.py
@@ -11,3 +11,4 @@ def P(psi, k):
     p_psi_k = 1j * k * psi_k  # Apply the momentum operator in k-space
     p_psi = np.fft.ifftn(p_psi_k)  # Transform back to position space
     return -1j*np.real(p_psi)
+
